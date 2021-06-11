41e2a98724daa519d546d7f70db1852648d24a5d
user.email <radu.m10@protonmail.com>
2021-06-11 23:10:46 +0300

    mouse accel disable script

--- extras/update_configs.sh
+++ extras/update_configs.sh
@@ -3,3 +3,10 @@
 cat ./pacman.conf > /etc/pacman.conf
 cat ./pipewire.conf > /usr/share/pipewire/pipewire.conf
 cat ./Xsession > /etc/lightdm/Xsession
+
+if [ -f /etc/X11/xorg.conf.d/50-mouse-acceleration.conf ];then
+    cat ./50-mouse-acceleration.conf > /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+else
+    touch /etc/X11/xorg.conf.d/50-mouse-acceleration.conf && cat ./50-mouse-acceleration.conf > /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+fi
+
