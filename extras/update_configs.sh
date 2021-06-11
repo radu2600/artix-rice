3345e754c04b2f3879ebb0d6431754ddc16679ef
user.email <radu.m10@protonmail.com>
2021-06-11 23:17:53 +0300

    nvidia persistent settings

--- extras/update_configs.sh
+++ extras/update_configs.sh
@@ -10,3 +10,8 @@ else
     touch /etc/X11/xorg.conf.d/50-mouse-acceleration.conf && cat ./50-mouse-acceleration.conf > /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
 fi
 
+if [ -f /etc/X11/xinit/xinitrc.d/95-nvidia-settings ];then
+    cat ./95-nvidia-settings > /etc/X11/xinit/xinitrc.d/95-nvidia-settings
+else
+    touch /etc/X11/xinit/xinitrc.d/95-nvidia-settings && cat ./95-nvidia-settings > /etc/X11/xinit/xinitrc.d/95-nvidia-settings
+fi
