443bb82c8ac09841e59eedabd0b083ae07f60efa
user.email <radu.m10@protonmail.com>
2022-02-27 18:43:43 +0200

    SD

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -130,7 +130,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-acario-light)
+(setq doom-theme 'doom-1337)
 
 
 ;;ELFEED setup
