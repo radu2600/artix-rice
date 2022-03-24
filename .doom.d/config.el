0d912a525f90fa9f5e11c3a69165bd23797279e4
user.email <radu.m10@protonmail.com>
2022-03-24 12:39:02 +0200

    emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,6 +1,6 @@
 (blink-cursor-mode t)
 
-(setq doom-font (font-spec :family "Cascadia Mono" :size 18 :weight 'Regular)
+(setq doom-font (font-spec :family "FantasqueSansMono Nerd Font" :size 22 :weight 'Regular)
       doom-big-font (font-spec :family "mononoki Nerd Font"  :size 21))
 ;;(setq doom-themes-enable-bold nil)
 
