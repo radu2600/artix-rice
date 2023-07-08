dd831fe2dab3d2a8615205b01bcc05830a6a5a8b
Radu <rdmc10@github.com>
2023-07-08 17:10:53 +0300

    bspwm

--- export.sh
+++ export.sh
@@ -2,15 +2,14 @@
 
 
 cp $HOME/.config/i3/config ./.config/i3/config
+cp $HOME/.config/bspwm/bspwmrc ./.config/bspwm/bspwmrc
 cp $HOME/.config/nvim/init.vim ./.config/nvim/init.vim
-cp $HOME/.config/ranger/rc.conf ./.config/ranger/rc.conf
 cp $HOME/.config/gtk-3.0/settings.ini ./.config/gtk-3.0/settings.ini
-cp $HOME/.config/zathura/zathurarc ./.config/zathura/zathurarc
 cp $HOME/.config/alacritty/alacritty.yml ./.config/alacritty/alacritty.yml
 cp $HOME/.config/picom.conf ./.config/picom.conf
 cp $HOME/.zshrc ./.zshrc
-cp $HOME/.tmux.conf ./.tmux.conf
 cp $HOME/.bashrc ./.bashrc
 cp $HOME/.config/qutebrowser/config.py ./.config/qutebrowser/config.py
 cp -rf $HOME/.doom.d ./
+cp -rf $HOME/.config/sxhkd/* ./.config/sxhkd/
 cp -rf $HOME/.config/polybar ./.config/
