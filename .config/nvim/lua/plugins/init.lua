5196f817a96414244346c7dd93f817d8306fa194
Radu <rdmc10@github.com>
2026-02-07 02:14:20 +0200

    Added lua nvim config

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary"
+    },
+    require("plugins.colors"),
+    require("plugins.telescope"),
+    require("plugins.treesitter"),
+    require("plugins.undotree"),
+    require("plugins.fugitive"),
+    require("plugins.lsp"),
+}
+
